bd13708123a2cb7b734bac0335b4ecf91542fbbf
AmitaJoshi <amitajoshi1992@gmail.com>
2020-12-17 15:41:51 +0530

    Update sp.groovy

--- vars/sp.groovy
+++ vars/sp.groovy
@@ -1,3 +1,18 @@
 def call(Map param){
-
+env.REPO_NAME= param.REPO_NAME
+env.BRANCH=param.BRANCH
+  pipeline
+  {
+    node
+    {
+      stage("checkout_scm") 
+      {
+      sh '''
+      git clone $REPO_NAME
+      cd nodesampleapp
+      git checkout $BRANCH
+      '''
+      }
+    }
+  }
 }
