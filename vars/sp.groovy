c9441979a17a28c322d596074f3e4c45545334d5
AmitaJoshi <amitajoshi1992@gmail.com>
2020-12-21 17:52:03 +0530

    Update sp.groovy

--- vars/sp.groovy
+++ vars/sp.groovy
@@ -16,20 +16,20 @@ env.BRANCH=param.BRANCH
       }
       stage("build")
       {
-        sh '''
+        /*sh '''
         cd nodesampleapp
           npm init --yes
           npm install eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
           npm install eslint eslint-config-airbnb-base eslint-plugin-import
-        '''
+        '''*/
         //add your build steps here
       }
       stage("Static Code Analysis")
       {
-        sh '''
+        /*sh '''
           cd nodesampleapp
           eslint app1.js
-        '''
+        '''*/
         //add your build steps here
       }
       stage("deploy_to_dev_env")
